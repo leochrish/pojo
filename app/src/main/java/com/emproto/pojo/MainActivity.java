ad5d458f5295548ecdd4f3c90177779825cf700c
leo <leoni.christopher@ergos.in>
2022-02-14 16:44:28 +0530

    removed pointzi

--- app/src/main/java/com/emproto/pojo/MainActivity.java
+++ app/src/main/java/com/emproto/pojo/MainActivity.java
@@ -4,9 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.emproto.pojo.pojo.api.ApiDataSource;
 import com.emproto.pojo.pojo.utility.PojoObserver;
-import com.pointzi.Pointzi;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,7 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Pointzi.setUserId("leoni@emproto.com");
         PojoObserver pojoObserver = new PojoObserver(this);
     }
 }
